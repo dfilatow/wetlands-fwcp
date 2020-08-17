f65f24c7fdf785dd6e7159f3596a17d4d7b2e515
Hunter Gleason <Hunter.Gleason@gov.bc.ca>
2020-08-17 14:48:38 -0700

    made some comments and minor changes to bcdc_fasterize, added a HG dev scripts directory to repo ...'

---
+++ HG_Scripts/hg_wetland_dev.R
@@ -0,0 +1,53 @@
+
+#Fill sinks in a local SRTM DEM, name 'dem_fill'
+RSAGA::rsaga.fill.sinks('/home/hunter/Downloads/dem.sgrd','/home/hunter/Downloads/dem_fill.sgrd',method="wang.liu.2006")
+
+
+#Function return catchment scale statistics provided a DEM, x-y pour point coordinates (map units), grid(s) to derive stats from, and desired statistic(s). 
+get_basin_stats<-function(dem_pth,x,y,agg_grids,stat)
+{
+  #Get up-slope catchment area for the point defined by 'x,y', derived from filled DEM using MFD algorithm in SAGA 
+  RSAGA::rsaga.geoprocessor('ta_hydrology',4,param = list(TARGET_PT_X=x,TARGET_PT_Y=y,ELEVATION=dem_pth,AREA=paste(tempdir(),'/basin.sgrd',sep=""),METHOD=2))
+  
+  #Using output from up-slope catchment area, convert to binary grid where MFD contributing is > 50%
+  RSAGA::rsaga.grid.calculus(in.grids = paste(tempdir(),'/basin.sgrd',sep=""), out.grid = paste(tempdir(),'/basin_bnry.sgrd',sep=""), formula = 'gt(a,50)')
+  
+  #Calculate zonal statistics using binary output grid as zones (1 corresponds to up-slope catchment), save stats to CSV
+  RSAGA::rsaga.geoprocessor('statistics_grid',5,param = list(ZONES=paste(tempdir(),'/basin_bnry.sgrd',sep=""),STATLIST=agg_grids,OUTTAB=paste(tempdir(),'/basin_stats.csv',sep="")))
+  
+  #Read in zonal statistics results, return desired statistics. 
+  rslt_tab<-read.csv(paste(tempdir(),'/basin_stats.csv',sep=""))
+  if(stat=="tabl")
+  {
+    return(rslt_tab)
+  }
+  if(stat=="count")
+  {
+    return(rslt_tab[2,3])
+  }
+  if(stat=="min")
+  {
+    return(rslt_tab[2,4])
+  }
+  if(stat=="max")
+  {
+    return(rslt_tab[2,5])
+  }
+  if(stat=="mean")
+  {
+    return(rslt_tab[2,6])
+  }
+  if(stat=="sd")
+  {
+    return(rslt_tab[2,7])
+  }
+  if(stat=="sum")
+  {
+    return(rslt_tab[2,8])
+  }
+}
+
+#An example, for basin defined by point (EPSG:3005; x:1299974,y:954998)
+start<-Sys.time()
+hm<-get_basin_stats('/home/hunter/Downloads/dem_fill.sgrd',1299974,954998,'/home/hunter/Downloads/dem.sgrd',"tabl")
+end<-Sys.time()
--- bcdc_fasterize.R
+++ bcdc_fasterize.R
@@ -6,21 +6,41 @@
 ## and a second to fasterize.bcdc.poly
 #ms <- ms.ws %>% filter(MAP_TILE == mapsheet)
 fasterize.sn <- function(reference.raster, mapsheet = "093J070", buf = (sum(res(raster.ref)/2))) {
+  
   library (raster)
   library (fasterize)
   library (bcdata)
   
+  bcalb <- '+proj=aea +lat_1=50 +lat_2=58.5 +lat_0=45 +lon_0=-126 +x_0=1000000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
+  
+  #Obtain matching BC mapsheet tiles.
   ms <- bcdc_query_geodata("a61976ac-d8e8-4862-851e-d105227b6525", crs = bcalb) %>%
     filter( MAP_TILE == mapsheet) %>%
     collect()
+  
+  #Get Freshwater Atlas Stream Network and intersect with mapsheet tiles. 
   sn <- bcdc_query_geodata("92344413-8035-4c08-b996-65a9b3f62fca", crs = bcalb) %>%
     filter( INTERSECTS(ms)) %>%
-    collect() %>% st_as_sf()
-  sn.ms <- st_intersection(sn, ms)
-  dem.ms <- dem.ws %>% raster::crop(ms) %>% raster::mask(ms)
-  sn.ms.r <- sn.ms %>% st_buffer(dist=buf) %>% 
-    st_union() %>% st_as_sf() %>% 
-    fasterize(raster.ref)
+    collect() %>% 
+    st_as_sf()
+  
+  #Hmm, may not have to do this?
+  #sn.ms <- st_intersection(sn, ms)
+  
+  #!!is dem.ms defined??#
+  # dem.ms <- dem.ws %>% 
+  #   raster::crop(ms) %>% 
+  #   raster::mask(ms)
+  
+  #Should raster.ref be 'reference.raster'?
+  # sn.ms.r <- sn.ms %>% 
+  #   st_buffer(dist=buf) %>% 
+  #   st_union() %>% 
+  #   st_as_sf() %>% 
+  #   fasterize(raster.ref)
+  
+  
   return(sn.ms.r)
 }
+
 sn.ms.r <- fasterize.sn(mapsheet, ww.ms, 25)
